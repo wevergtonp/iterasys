dc4adc5aeacd2a299ba68fe0f39156e1cfa3e212
Wevergton Pereira <florentinowevergton@gmail.com>
2021-07-08 09:11:25 -0300

    Adicionei uma mensagem de "Boa noite!!!" no método principal da classe Medidas.
    Troquei o nome do método "CalcularModoCompacto" para CalcularModoCurto".
    
    Adicionei quebra de linha no método principal para separar as saídas de tela dos métodos.

--- _iterasys/src/main/java/intro/Medidas.java
+++ _iterasys/src/main/java/intro/Medidas.java
@@ -9,30 +9,32 @@ public class Medidas {
 
     // 3.2 - Métodos e Funções
     public static void main(String[] args) {
-        System.out.println("Calculo de áreas");
-        System.out.println("Boa noite!!!");
-        calcularModoCompacto();
+        System.out.println("Bom dia!!!");
+        calcularModoCurto();
+        System.out.println();
+        calcularAreaModoExtenso();
     }
 
     //Métodos de apoio
 
     public static void calcularAreaModoExtenso(){
         //Calculo de área - Exemplo: o tamanho do tapete ou do piso
+        System.out.println("Calculando área por modo extenso");
         int largura;
         int comprimento;
         int resultado;
 
         largura = 5;
-        comprimento = 6;
+        comprimento = 5;
         resultado = largura * comprimento;
 
         //Saída na tela
-        System.out.println("A área do tapete é de: " + resultado + "m²");
         System.out.println("Para a largura de " + largura + "m e o comprimento de " + comprimento +
                 "m. A área é de " + resultado + "m²");
     }
 
-    public static void calcularModoCompacto(){
+    public static void calcularModoCurto(){
+        System.out.println("Calculando área por modo compacto");
         int largura = 5;
         int comprimento = 6;
 
